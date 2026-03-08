12e0e5ab36972c5693f6ecb647b775da518811c3
Anitej Isaac Sharma <aisaaxs@Anitejs-MacBook-Pro-2.local>
2026-03-07 20:45:35 -0800

    fix for prisma client import

--- backend/src/prisma.service.ts
+++ backend/src/prisma.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
-import { PrismaClient } from 'generated/prisma/client';
+import { PrismaClient } from '../generated/prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 @Injectable()
