a7a48244e12408dcfc3147d6bd6db56aa3071faf
Anitej Isaac Sharma <anitejsharmas@gmail.com>
2026-07-08 22:55:04 -0700

    cleanup

--- app/(test routes)/test-loading/page.tsx
+++
@@ -1,9 +0,0 @@
-async function wait(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-export default async function TestLoadingPage() {
-  await wait(3000);
-
-  return <div>Loaded</div>;
-}
--- app/products/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Products() {
-    return (
-        <main className="min-h-screen overflow-hidden bg-black text-white flex justify-center items-center">
-            <p className="text-4xl font-bold italic capitalize">coming soon...</p>
-        </main>
-    )
-}
